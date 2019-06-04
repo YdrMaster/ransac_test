9c93545c3fc713c5c2e45266a016927c60af77dd
YangDR <ydrml@hotmail.com>
2019-06-04 14:30:20 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,66 +1,5 @@
 ﻿#include <iostream>
-#include <vector>
-#include <random>
-
-#include "model.hpp"
-#include "random_engine.hpp"
-
-// 迭代参数
-constexpr auto P = 0.3f; // 判断模型是否适用于数据的比例
-
-template<class _model_t>
-void ransac(const std::vector<typename _model_t::_point_t> &data,
-            float threshold,
-            unsigned int max_times) {
-    using tp = typename _model_t::_point_t;
-    using ti = decltype(data.size());
-    
-    if (data.size() < 2 * _model_t::size_to_make)
-        throw std::logic_error("samples too little");
-    
-    // 随机数引擎
-    random_engine<decltype(data.size())>
-        random(0, data.size() - 1);
-    // 模型初始化器
-    std::array<tp, _model_t::size_to_make>
-        initialize_list{};
-    // 缓存
-    std::vector<bool>
-        check_buffer(data.size());
-    
-    _model_t best_model{}, model{};
-    size_t   best_count = 0;
-    
-    for (ti i = 0; i < max_times; ++i) {
-        for (ti j = 0; j < initialize_list.size(); ++j)
-            initialize_list[j] = data[random()];
-        
-        model.make(initialize_list);
-        if (!model.valid() || (best_model.valid() && (model.normalize(), model == best_model)))
-            continue;
-        
-        std::transform(data.begin(), data.end(),
-                       check_buffer.begin(),
-                       [&](const tp &point) {
-                           return std::abs(model(point)) < threshold;
-                       });
-        
-        size_t    count = 0;
-        for (auto r : check_buffer)
-            if (r) ++count;
-        
-        if (count > best_count) {
-            best_model = model;
-            best_count = count;
-            best_model.normalize();
-        }
-    }
-    
-    std::cout << best_model.a() << " "
-              << best_model.b() << " "
-              << best_model.c() << " "
-              << best_count << std::endl;
-}
+#include "ransac.h"
 
 int main() {
     std::vector<line_t::_point_t> points{
@@ -69,10 +8,17 @@ int main() {
         {{2, 2}},
         {{3, 6}},
         {{4, 4}},
-        {{4, 4}},
+        {{5, 5}},
     };
     
-    ransac<line_t>(points, .1f, 10);
+    auto result = ransac<line_t>(points, .1f, 10);
+    
+    std::cout << result.model.a() << "x + "
+              << result.model.b() << "y + "
+              << result.model.c() << " = 0" << std::endl;
+    
+    for (auto i : result.inliers)
+        std::cout << points[i].x() << ", " << points[i].y() << std::endl;
     
     return 0;
 }
--- ransac.h
+++ ransac.h
@@ -1,8 +1,77 @@
-//
+﻿//
 // Created by User on 2019/6/4.
 //
 
 #ifndef RANSAC_TEST_RANSAC_H
 #define RANSAC_TEST_RANSAC_H
+
+
+#include "model.hpp"
+#include "random_engine.hpp"
+
+template<class _model_t>
+struct ransac_result {
+    _model_t            model;
+    std::vector<size_t> inliers;
+};
+
+template<class _model_t>
+ransac_result<_model_t> ransac(
+    const std::vector<typename _model_t::_point_t> &data,
+    float threshold,
+    unsigned int max_times
+) {
+    using tp = typename _model_t::_point_t;
+    
+    if (data.size() < 2 * _model_t::size_to_make)
+        throw std::logic_error("samples too little");
+    
+    // 随机数引擎
+    random_engine<decltype(data.size())>
+        random(0, data.size() - 1);
+    // 模型初始化器
+    std::array<tp, _model_t::size_to_make>
+        initialize_list{};
+    // 缓存
+    std::vector<bool>
+        check_buffer(data.size());
+    // 局内点
+    std::vector<size_t>
+        inliers{};
+    
+    _model_t best_model{}, model{};
+    size_t   best_count = 0;
+    
+    for (size_t i = 0; i < max_times; ++i) {
+        for (size_t j = 0; j < initialize_list.size(); ++j)
+            initialize_list[j] = data[random()];
+        
+        model.make(initialize_list);
+        if (!model.valid())
+            continue;
+        model.normalize();
+        if (best_model.valid() && model == best_model)
+            continue;
+        
+        std::transform(data.begin(), data.end(),
+                       check_buffer.begin(),
+                       [&](const tp &point) {
+                           return std::abs(model(point)) < threshold;
+                       });
+        
+        size_t count = std::count(check_buffer.begin(), check_buffer.end(), true);
+        if (count <= inliers.size()) continue;
+        
+        best_model = model;
+        inliers.resize(count);
+        
+        auto        ptr = inliers.begin();
+        for (size_t i   = 0; i < check_buffer.size(); ++i)
+            if (check_buffer[i]) *ptr++ = i;
+    }
+    
+    return {best_model, inliers};
+}
+
 
 #endif //RANSAC_TEST_RANSAC_H
